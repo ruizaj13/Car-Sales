24d31126153e9071ed6ae169ce8ea8905aa734c1
ruizaj13 <ruizaj13@gmail.com>
2020-12-08 21:51:36 -0800

    done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,13 @@
 //action constant
 export const ADD_FEATURE = 'ADD_FEATURE';
+export const REMOVE_FEATURE = 'REMOVE_FEATURE'
 
 //action creator function
 //use this when dispatching 
-export const addFeature = feature => {
-    return({type: ADD_FEATURE, payload:feature})
+export const addFeature = (newFeature) => {
+    return({type: ADD_FEATURE, payload:newFeature})
+}
+
+export const removeFeature = (feature) => {
+    return({ type: REMOVE_FEATURE, payload:feature})
 }
